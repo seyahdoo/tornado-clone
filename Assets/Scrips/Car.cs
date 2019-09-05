dc41dfc22b3acbfb889fe6c8918c380f3f9f2ab6
Seyyid Ahmed Doğan <contact@sadogan.com>
2019-09-05 19:53:33 +0300

    Car Movement

--- Assets/Scrips/Car.cs
+++ Assets/Scrips/Car.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public CarPath path;
+
+    public float speed = 1f;
+    public float guideDistance = 1f;
+
+
+    public int checkpointCount = 0;
+    public Transform guide;
+
+    public Transform tr;
+
+    private void Start()
     {
-        
+        guide.position = tr.position;
+        guide.LookAt(path.checkpoints[checkpointCount].transform);
+        guide.position += guide.forward * guideDistance;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Transform target = path.checkpoints[checkpointCount].transform;
+        if (Vector3.Distance(target.position, guide.position) < (speed * Time.deltaTime))
+        {
+            checkpointCount += 1;
+            checkpointCount %= path.checkpoints.Length;
+        }
+
+        guide.LookAt(target);
+        guide.position += guide.forward * speed * Time.deltaTime;
+
+        tr.LookAt(guide);
+        tr.position += tr.forward * speed * Time.deltaTime;
     }
+
+
+
+
 }
