f753c05cbd686a44288ae7bc4d45805bd1c59959
Seyyid Ahmed Doğan <contact@sadogan.com>
2019-09-05 18:11:40 +0300

    ground prefabs added

---
+++ Assets/Scrips/LevelOptimizer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOptimizer : MonoBehaviour
+{
+    
+    //Open All Levels, Change all Throwable objects to throwable slots that can be populated by PoolDirector or LevelDirector.
+
+}
